c8f356ccb660f7c5c794c914e73e48562f532de1
H4ad <H4ad@users.noreply.github.com>
2022-06-19 16:08:55 +0000

    deploy: 2e56cae962ca3a6e14761c2d51ac9903025dd39c

--- assets/js/814f3328.1eaace99.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[2535],{5641:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"The Beginning","permalink":"/serverless-adapter/blog/the-beginning"}]}')}}]);
--- assets/js/e3705365.9344e7ea.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[2331],{3718:function(e){e.exports=JSON.parse('{"permalink":"/serverless-adapter/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
