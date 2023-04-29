c0bc717f21d3ab13cc7e922deae8994996a72a5f
H4ad <H4ad@users.noreply.github.com>
2023-04-29 03:45:56 +0000

    deploy: d98fddd98cdd3b3b1d5db5deb6bc58986dd681bd

---
+++ assets/js/02dae591.285f5a34.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[2161],{3637:s=>{s.exports=JSON.parse('{"permalink":"/blog/tags/aws","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
---
+++ assets/js/41f11d2e.f5869e4f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[2562],{8413:s=>{s.exports=JSON.parse('{"label":"serverless-adapter","permalink":"/blog/tags/serverless-adapter","allTagsPath":"/blog/tags","count":3}')}}]);
