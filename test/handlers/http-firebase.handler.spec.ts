3a451cfc627ef1d78d6b0826d864f04576c2a2c6
Vinícius Lourenço <vinicius@signoz.io>
2026-06-01 20:09:26 -0300

    feat(apollo-server): drop support to v4

--- test/handlers/http-firebase.handler.spec.ts
+++ test/handlers/http-firebase.handler.spec.ts
@@ -1,6 +1,5 @@
 import { describe, expect, it, vitest } from 'vitest';
 import type { Request, Response } from 'express';
-import semver from 'semver';
 import {
   type FrameworkContract,
   ServerlessRequest,
@@ -9,99 +8,94 @@ import {
 } from '../../src';
 import { FrameworkMock } from '../mocks/framework.mock';
 
-describe.skipIf(semver.gte(process.versions.node, '25.0.0'))(
-  'HttpFirebaseHandler',
-  () => {
-    it('should forward correctly the request to framework', async () => {
-      const { HttpFirebaseHandler } =
-        await import('../../src/handlers/firebase');
-      const handlerFactory = new HttpFirebaseHandler<null>();
+describe('HttpFirebaseHandler', () => {
+  it('should forward correctly the request to framework', async () => {
+    const { HttpFirebaseHandler } = await import('../../src/handlers/firebase');
+    const handlerFactory = new HttpFirebaseHandler<null>();
 
-      const method = 'POST';
-      const url = '/users/batata';
-      const headers = { 'Content-Type': 'application/json' };
-      const remoteAddress = '168.16.0.1';
-      const body = Buffer.from('{"test": true}', 'utf-8');
+    const method = 'POST';
+    const url = '/users/batata';
+    const headers = { 'Content-Type': 'application/json' };
+    const remoteAddress = '168.16.0.1';
+    const body = Buffer.from('{"test": true}', 'utf-8');
 
+    const request = new ServerlessRequest({
+      method,
+      url,
+      headers,
+      remoteAddress,
+      body,
+    });
+
+    const response = new ServerlessResponse({
+      method,
+    });
+
+    const responseBody = { batata: true };
+    const responseStatus = 200;
+    const framework = new FrameworkMock(responseStatus, responseBody);
+
+    const handler = handlerFactory.getHandler(null, framework);
+
+    handler(request as Request, response as unknown as Response);
+
+    await waitForStreamComplete(response);
+
+    expect(response.statusCode).toBe(responseStatus);
+    expect(ServerlessResponse.body(response).toString()).toStrictEqual(
+      JSON.stringify(responseBody),
+    );
+  });
+
+  it('should handle weird body types', async () => {
+    const { HttpFirebaseHandler } = await import('../../src/handlers/firebase');
+    const handlerFactory = new HttpFirebaseHandler();
+
+    const method = 'POST';
+    const url = '/users/batata';
+    const headers = { 'Content-Type': 'application/json' };
+    const remoteAddress = '168.16.0.1';
+    const options = [{ potato: true }, [{ test: true }]];
+
+    for (const option of options) {
       const request = new ServerlessRequest({
         method,
         url,
         headers,
         remoteAddress,
-        body,
+        body: option as any,
       });
 
       const response = new ServerlessResponse({
         method,
       });
 
-      const responseBody = { batata: true };
-      const responseStatus = 200;
-      const framework = new FrameworkMock(responseStatus, responseBody);
+      const framework: FrameworkContract<unknown> = {
+        sendRequest: vitest.fn(
+          async (
+            _app: null,
+            req: ServerlessRequest,
+            res: ServerlessResponse,
+          ) => {
+            expect(req.body?.toString()).toEqual(JSON.stringify(option));
+            expect(req.headers['content-length']).toEqual(
+              Buffer.byteLength(JSON.stringify(option)).toString(),
+            );
+
+            req.pipe(res);
+
+            await waitForStreamComplete(res);
+
+            expect(ServerlessResponse.body(res).toString()).toEqual(
+              JSON.stringify(option),
+            );
+          },
+        ),
+      };
 
       const handler = handlerFactory.getHandler(null, framework);
 
       handler(request as Request, response as unknown as Response);
-
-      await waitForStreamComplete(response);
-
-      expect(response.statusCode).toBe(responseStatus);
-      expect(ServerlessResponse.body(response).toString()).toStrictEqual(
-        JSON.stringify(responseBody),
-      );
-    });
-
-    it('should handle weird body types', async () => {
-      const { HttpFirebaseHandler } =
-        await import('../../src/handlers/firebase');
-      const handlerFactory = new HttpFirebaseHandler();
-
-      const method = 'POST';
-      const url = '/users/batata';
-      const headers = { 'Content-Type': 'application/json' };
-      const remoteAddress = '168.16.0.1';
-      const options = [{ potato: true }, [{ test: true }]];
-
-      for (const option of options) {
-        const request = new ServerlessRequest({
-          method,
-          url,
-          headers,
-          remoteAddress,
-          body: option as any,
-        });
-
-        const response = new ServerlessResponse({
-          method,
-        });
-
-        const framework: FrameworkContract<unknown> = {
-          sendRequest: vitest.fn(
-            async (
-              _app: null,
-              req: ServerlessRequest,
-              res: ServerlessResponse,
-            ) => {
-              expect(req.body?.toString()).toEqual(JSON.stringify(option));
-              expect(req.headers['content-length']).toEqual(
-                Buffer.byteLength(JSON.stringify(option)).toString(),
-              );
-
-              req.pipe(res);
-
-              await waitForStreamComplete(res);
-
-              expect(ServerlessResponse.body(res).toString()).toEqual(
-                JSON.stringify(option),
-              );
-            },
-          ),
-        };
-
-        const handler = handlerFactory.getHandler(null, framework);
-
-        handler(request as Request, response as unknown as Response);
-      }
-    });
-  },
-);
+    }
+  });
+});
