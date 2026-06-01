e95a98ec6b9b9be70d49eb2f49b4c19d9b161dc4
Vinícius Lourenço <vinicius@signoz.io>
2026-06-01 20:06:35 -0300

    feat(firebase-functions): drop support for 4,5,6

--- test/handlers/http-firebase-v2.handler.spec.ts
+++ test/handlers/http-firebase-v2.handler.spec.ts
@@ -1,5 +1,4 @@
 import type { HttpsOptions } from 'firebase-functions/v2/https';
-import semver from 'semver';
 import { describe, expect, it, vitest } from 'vitest';
 import {
   type FrameworkContract,
@@ -9,114 +8,111 @@ import {
 } from '../../src';
 import { FrameworkMock } from '../mocks/framework.mock';
 
-describe.skipIf(semver.gte(process.versions.node, '25.0.0'))(
-  'HttpFirebaseV2Handler',
-  () => {
-    it('should forward correctly the request to framework', async () => {
-      const { HttpFirebaseV2Handler } =
-        await import('../../src/handlers/firebase');
-      const handlerFactory = new HttpFirebaseV2Handler();
+describe('HttpFirebaseV2Handler', () => {
+  it('should forward correctly the request to framework', async () => {
+    const { HttpFirebaseV2Handler } =
+      await import('../../src/handlers/firebase');
+    const handlerFactory = new HttpFirebaseV2Handler();
 
-      const method = 'POST';
-      const url = '/users/batata';
-      const headers = { 'Content-Type': 'application/json' };
-      const remoteAddress = '168.16.0.1';
-      const body = Buffer.from('{"test": true}', 'utf-8');
+    const method = 'POST';
+    const url = '/users/batata';
+    const headers = { 'Content-Type': 'application/json' };
+    const remoteAddress = '168.16.0.1';
+    const body = Buffer.from('{"test": true}', 'utf-8');
 
+    const request = new ServerlessRequest({
+      method,
+      url,
+      headers,
+      remoteAddress,
+      body,
+    });
+
+    const response = new ServerlessResponse({
+      method,
+    });
+
+    const responseBody = { batata: true };
+    const responseStatus = 200;
+    const framework = new FrameworkMock(responseStatus, responseBody);
+
+    const handler = handlerFactory.getHandler(null, framework);
+
+    handler(request, response);
+
+    await waitForStreamComplete(response);
+
+    expect(response.statusCode).toBe(responseStatus);
+    expect(ServerlessResponse.body(response).toString()).toStrictEqual(
+      JSON.stringify(responseBody),
+    );
+  });
+
+  it('should handle weird body types', async () => {
+    const { HttpFirebaseV2Handler } =
+      await import('../../src/handlers/firebase');
+    const handlerFactory = new HttpFirebaseV2Handler();
+
+    const method = 'POST';
+    const url = '/users/batata';
+    const headers = { 'Content-Type': 'application/json' };
+    const remoteAddress = '168.16.0.1';
+    const options = [{ potato: true }, [{ test: true }]];
+
+    for (const option of options) {
       const request = new ServerlessRequest({
         method,
         url,
         headers,
         remoteAddress,
-        body,
+        body: option as any,
       });
 
       const response = new ServerlessResponse({
         method,
       });
 
-      const responseBody = { batata: true };
-      const responseStatus = 200;
-      const framework = new FrameworkMock(responseStatus, responseBody);
+      const framework: FrameworkContract<unknown> = {
+        sendRequest: vitest.fn(
+          async (
+            _app: null,
+            req: ServerlessRequest,
+            res: ServerlessResponse,
+          ) => {
+            expect(req.body?.toString()).toEqual(JSON.stringify(option));
+            expect(req.headers['content-length']).toEqual(
+              Buffer.byteLength(JSON.stringify(option)).toString(),
+            );
+
+            req.pipe(res);
+
+            await waitForStreamComplete(res);
+
+            expect(ServerlessResponse.body(res).toString()).toEqual(
+              JSON.stringify(option),
+            );
+          },
+        ),
+      };
 
       const handler = handlerFactory.getHandler(null, framework);
 
       handler(request, response);
+    }
+  });
 
-      await waitForStreamComplete(response);
+  it('should forward the properties to https.onRequest', async () => {
+    const { HttpFirebaseV2Handler } =
+      await import('../../src/handlers/firebase');
+    const options: HttpsOptions = {
+      concurrency: 400,
+    };
+    const factory = new HttpFirebaseV2Handler(options);
 
-      expect(response.statusCode).toBe(responseStatus);
-      expect(ServerlessResponse.body(response).toString()).toStrictEqual(
-        JSON.stringify(responseBody),
-      );
-    });
+    const spyMethod = vitest.spyOn(factory, 'onRequestWithOptions' as any);
 
-    it('should handle weird body types', async () => {
-      const { HttpFirebaseV2Handler } =
-        await import('../../src/handlers/firebase');
-      const handlerFactory = new HttpFirebaseV2Handler();
+    factory.getHandler(null, new FrameworkMock(200, {}));
 
-      const method = 'POST';
-      const url = '/users/batata';
-      const headers = { 'Content-Type': 'application/json' };
-      const remoteAddress = '168.16.0.1';
-      const options = [{ potato: true }, [{ test: true }]];
-
-      for (const option of options) {
-        const request = new ServerlessRequest({
-          method,
-          url,
-          headers,
-          remoteAddress,
-          body: option as any,
-        });
-
-        const response = new ServerlessResponse({
-          method,
-        });
-
-        const framework: FrameworkContract<unknown> = {
-          sendRequest: vitest.fn(
-            async (
-              _app: null,
-              req: ServerlessRequest,
-              res: ServerlessResponse,
-            ) => {
-              expect(req.body?.toString()).toEqual(JSON.stringify(option));
-              expect(req.headers['content-length']).toEqual(
-                Buffer.byteLength(JSON.stringify(option)).toString(),
-              );
-
-              req.pipe(res);
-
-              await waitForStreamComplete(res);
-
-              expect(ServerlessResponse.body(res).toString()).toEqual(
-                JSON.stringify(option),
-              );
-            },
-          ),
-        };
-
-        const handler = handlerFactory.getHandler(null, framework);
-
-        handler(request, response);
-      }
-    });
-
-    it('should forward the properties to https.onRequest', async () => {
-      const { HttpFirebaseV2Handler } =
-        await import('../../src/handlers/firebase');
-      const options: HttpsOptions = {
-        concurrency: 400,
-      };
-      const factory = new HttpFirebaseV2Handler(options);
-
-      const spyMethod = vitest.spyOn(factory, 'onRequestWithOptions' as any);
-
-      factory.getHandler(null, new FrameworkMock(200, {}));
-
-      expect(spyMethod).toHaveBeenCalledWith(options, expect.any(Function));
-    });
-  },
-);
+    expect(spyMethod).toHaveBeenCalledWith(options, expect.any(Function));
+  });
+});
